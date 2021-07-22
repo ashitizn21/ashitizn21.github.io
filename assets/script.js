ba18fffa8397ba8c78a4ee85f10bb0a3f138edf6
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2021-07-22 18:06:21 +0300

    Home page with its style

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navBar").addClass("sticky");
+        } else{
+            $(".navBar").removeClass("sticky");
+        }
+    })
 
+    $(".menu-btn").click(function(){
+        $(".navBar .menu").toggleClass("active");
+
+        $(".menu-btn i").toggleClass("active");
+    });
    
 });
