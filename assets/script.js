225131a0002411fd4800369be7974c40b71767b8
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2021-07-26 01:14:10 +0300

    Lorem ipsum dolor sit, amet consectetur adipisicing elit.

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,13 @@ $(document).ready(function(){
         } else{
             $(".navBar").removeClass("sticky");
         }
-    })
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
     $(".menu-btn").click(function(){
         $(".navBar .menu").toggleClass("active");
@@ -13,4 +19,24 @@ $(document).ready(function(){
         $(".menu-btn i").toggleClass("active");
     });
    
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Competitive Programmer", "App Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing_2", {
+        strings: ["Web Developer", "Competitive Programmer", "App Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.scroll-up-btn').click(function(){  
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
 });
